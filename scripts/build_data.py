1014541ad5a704379137611783c22a6e894fd58b
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-06-24 00:57:10 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -1,8 +1,10 @@
 from sdg.open_sdg import open_sdg_build
 
-def alter_meta(meta):
+def alter_meta(meta, context):
     if 'reporting_status' in meta:
         del meta['reporting_status']
+    if context['indicator_id']:
+        print(context('indicator_id')
     return meta
 
 open_sdg_build(config='config_data.yml', alter_meta=alter_meta)
