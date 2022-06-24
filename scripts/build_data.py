6608d6ddd35c630d01e3842086b6fc0871a1dc12
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-06-24 01:31:29 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -8,7 +8,8 @@ def alter_meta(meta, context):
     return meta
 
 def alter_data(df):
-    print(df)
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        print(df)
     return df
     
 
