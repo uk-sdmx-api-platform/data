39d2033aa6ece1c6513b1eed7d9fbec5dc32beb8
Lucy <43404358+LucyGwilliamAdmin@users.noreply.github.com>
2022-06-28 00:48:37 +0100

    Update build_data.py

--- scripts/build_data.py
+++ scripts/build_data.py
@@ -9,17 +9,24 @@ def alter_meta(meta, context):
         print(context['indicator_id'])
     return meta
 
-def alter_data(df):
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        print(df)
+def alter_data(df, context):
+    if context['indicator_id'] == '1-1-1'
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+            print(df)
     if 'Reference area' in df.columns:
         df["Reference area"].replace({"United Kingdom of Great Britain and Northern Ireland":""}, inplace=True)
         if df['Reference area'].replace(r'^\s*$', np.nan, regex=True).isna().all():
             df = df.drop('Reference area', 1)
+        if context['indicator_id'] == '1-1-1'
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+            print(df)
     if 'REF_AREA' in df.columns:
         df["REF_AREA"].replace({826:""}, inplace=True)
         if df['REF_AREA'].replace(r'^\s*$', np.nan, regex=True).isna().all():
             df = df.drop('REF_AREA', 1)
+        if context['indicator_id'] == '1-1-1'
+        with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+            print(df)
     return df
     
 
